61e5c88c58770f66ca55ce921d12c65fff302c43
veprogames <veprogames.gamedev@gmail.com>
2022-08-12 18:05:19 +0200

    updaate mapgen code

--- mods/mapgen/init.lua
+++ mods/mapgen/init.lua
@@ -3,6 +3,33 @@ local c_active_coal = minetest.get_content_id("minelights:active_coal")
 local c_bedrock = minetest.get_content_id("bedrock:bedrock")
 local c_wood = minetest.get_content_id("default:wood")
 
+local chances = {
+    {content = c_active_coal, ymax = -5, ymin = -2000, chance = 0.015},
+    {content = c_wood, ymax = -1, ymin = -100, chance = 0.015},
+    {content = c_bedrock, ymax = -50, ymin = -250, chance = 0.02},
+    {content = c_bedrock, ymax = -250, ymin = -1000, chance = 0.04},
+    {content = c_bedrock, ymax = -1000, ymin = -1500, chance = 0.08},
+    {content = c_bedrock, ymax = -1500, ymin = -2000, chance = 0.2}
+}
+
+local function get_content_from_chances(y)
+    for i, chance in ipairs(chances) do
+        if y <= chance.ymax and y >= chance.ymin and math.random() < chance.chance then
+            return chance.content
+        end
+    end
+    return nil
+end
+
+local function get_content_mineral(y)
+    local tier = math.floor(1 - y / 20 + math.random() * 0.9)
+    if math.random() < 0.045 then
+        tier = tier + math.random(2, 4)
+    end
+    tier = math.min(100, tier)
+    return minetest.get_content_id("default:mineral"..tier)
+end
+
 minetest.register_on_generated(function(minp, maxp, blockseed)
     local voxelmanip, emin, emax = minetest.get_mapgen_object("voxelmanip")
     local area = VoxelArea:new({MinEdge = emin, MaxEdge = emax})
@@ -16,19 +43,13 @@ minetest.register_on_generated(function(minp, maxp, blockseed)
                     local cont
                     if y >= -1 then
                         cont = c_dirt
-                    elseif y < -5 and math.random() < 0.015 then
-                        cont = c_active_coal
-                    elseif y < -50 and math.random() < 0.03 then
+                    elseif y < -2022 then
                         cont = c_bedrock
-                    elseif y > -100 and math.random() < 0.015 * (1 - y / 100.0) then
-                        cont = c_wood
                     else
-                        local tier = math.floor(1 - y / 20 + math.random() * 0.9)
-                        if math.random() < 0.045 then
-                            tier = tier + math.random(2, 4)
+                        cont = get_content_from_chances(y)
+                        if cont == nil then
+                            cont = get_content_mineral(y)
                         end
-                        tier = math.min(100, tier)
-                        cont = minetest.get_content_id("default:mineral"..tier)
                     end
                     data[idx] = cont
                 end
