2b28543816ffe7b17f8cd749d32724fd10d82855
veprogames <veprogames.gamedev@gmail.com>
2022-09-19 22:19:13 +0200

    update mts_bignumber

--- mods/mts_bignumber/notations.lua
+++ mods/mts_bignumber/notations.lua
@@ -5,7 +5,8 @@ Notations = {
     GreekNotation = dofile(minetest.get_modpath("mts_bignumber").."/notations/greeknotation.lua"),
     HebrewNotation = dofile(minetest.get_modpath("mts_bignumber").."/notations/hebrewnotation.lua"),
     ScientificNotation = dofile(minetest.get_modpath("mts_bignumber").."/notations/scientificnotation.lua"),
-    EngineeringNotation = dofile(minetest.get_modpath("mts_bignumber").."/notations/engineeringnotation.lua")
+    EngineeringNotation = dofile(minetest.get_modpath("mts_bignumber").."/notations/engineeringnotation.lua"),
+    DynamicNotation = dofile(minetest.get_modpath("mts_bignumber").."/notations/dynamicnotation.lua")
 }
 
 return Notations
