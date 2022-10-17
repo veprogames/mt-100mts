3be43618b7695ce051424f6be4a7eb6b26e419e1
veprogames <veprogames.gamedev@gmail.com>
2022-10-17 20:32:38 +0200

    quick fix: remove power crystals on death

--- mods/mts_default/player.lua
+++ mods/mts_default/player.lua
@@ -4,7 +4,7 @@ minetest.register_on_dieplayer(function(player, reason)
     local items = inv:get_list("main")
     for k, item in pairs(items) do
         local definition = item:get_definition()
-        if definition._blacksmith_multiplier_id then
+        if definition._blacksmith_multiplier_id or definition._blacksmith_power_id then
             inv:remove_item("main", item)
         end
     end
