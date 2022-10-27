12a36bd386dcc27a4ae370e934b2b167e8400dde
veprogames <veprogames.gamedev@gmail.com>
2022-10-27 21:40:41 +0200

    add tutorial formspec remove mts_tutorial command

--- mods/mts_inventory/init.lua
+++ mods/mts_inventory/init.lua
@@ -1,6 +1,4 @@
-local modname = minetest.get_current_modname()
-local modpath = minetest.get_modpath(modname)
--- dofile(modpath .. "/foo.lua")
+Tutorial = dofile(minetest.get_modpath("mts_inventory").."/Tutorial.lua")
 
 local inventory_formspec = ""
 	.. "formspec_version[6]"
@@ -15,6 +13,7 @@ local inventory_formspec = ""
 	.. "listring[current_player;main]"
 	.. "listring[current_player;craft]"
 	.. "container_end[]"
+	.. "button[1,1;2,1;inventory_tutorial;Tutorial]"
 	.. "bgcolor[;neither;]"
 	.. "background[0,0;10.75,9.5;mts_inventory_inventory.png;false]"
 
@@ -27,4 +26,10 @@ end)
 minetest.register_on_newplayer(function(player)
 	minetest.chat_send_player(player:get_player_name(),
 		"Welcome to 100 Minerals to Success! Type "..minetest.colorize("#00ff00", "/mts_tutorial").." for an introduction")
+end)
+
+minetest.register_on_player_receive_fields(function(player, formname, fields)
+	if fields.inventory_tutorial then
+		Tutorial.show_formspec(player)
+	end
 end)
