ce322670578d308407ec7f9565a1de426f2e05f0
veprogames <veprogames.gamedev@gmail.com>
2022-10-29 10:31:09 +0200

    change tutorial background and add tooltip to recipe change some text

--- mods/mts_inventory/Tutorial.lua
+++ mods/mts_inventory/Tutorial.lua
@@ -4,7 +4,7 @@ function Tutorial.show_formspec(player)
     local text = [[1. Dig down and mine Stones
 2. Craft 5 Pebbles into a Rock
 3. Craft a Vortex Anvil out of 3 Rocks and 4 Pebble
-4. Mine Minerals and craft stronger Pickaxes
+4. Right Click the Blacksmith, let your Blacksmith absorb Minerals and craft stronger Pickaxes
 
 Avoid dying, as it will erase all minerals that are currently in your inventory]]
 
@@ -15,8 +15,9 @@ Avoid dying, as it will erase all minerals that are currently in your inventory]
     "style_type[label;font_size=*2;textcolor=#00ff00]"..
     "label[1,1;Tutorial]"..
     "style_type[label;font_size=*1]"..
-    string.format("textarea[1,1.7;5.5,8;;;%s]", text)..
+    string.format("textarea[1,1.7;5,8;;;%s]", text)..
     "container[6,1.7;4,4]"..
+        "tooltip[0,0;3,3;Blacksmith Crafting Recipe]"..
         "item_image[0,0;1,1;mts_default:rock]"..
         "item_image[1,0;1,1;mts_default:rock]"..
         "item_image[2,0;1,1;mts_default:rock]"..
